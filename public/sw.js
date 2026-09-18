5fe299659c57e205d764b0d9df03a9884a3a1bff
wjunior311-hash <wjunior311@gmail.com>
2026-09-18 00:09:34 -0300

    feat: add installable web app support

---
+++ public/sw.js
@@ -0,0 +1,4 @@
+const CACHE='tpbc-v1';
+self.addEventListener('install',e=>{self.skipWaiting()});
+self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim())});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
